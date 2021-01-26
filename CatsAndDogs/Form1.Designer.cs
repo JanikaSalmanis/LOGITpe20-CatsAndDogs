ce3faa9b60a748e91cbbf6c1b9198f32430511af
Opilane TTHK <opilane@tthk.ee>
2021-01-26 13:24:08 +0200

    first commit

--- CatsAndDogs/Form1.Designer.cs
+++ CatsAndDogs/Form1.Designer.cs
@@ -30,7 +30,10 @@
         {
             this.dogPicture = new System.Windows.Forms.PictureBox();
             this.GetADog = new System.Windows.Forms.Button();
+            this.catPicture = new System.Windows.Forms.PictureBox();
+            this.GetACat = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dogPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.catPicture)).BeginInit();
             this.SuspendLayout();
             // 
             // dogPicture
@@ -51,16 +54,37 @@
             this.GetADog.UseVisualStyleBackColor = true;
             this.GetADog.Click += new System.EventHandler(this.GetADog_Click);
             // 
+            // catPicture
+            // 
+            this.catPicture.Location = new System.Drawing.Point(12, 12);
+            this.catPicture.Name = "catPicture";
+            this.catPicture.Size = new System.Drawing.Size(436, 485);
+            this.catPicture.TabIndex = 3;
+            this.catPicture.TabStop = false;
+            // 
+            // GetACat
+            // 
+            this.GetACat.Location = new System.Drawing.Point(87, 519);
+            this.GetACat.Name = "GetACat";
+            this.GetACat.Size = new System.Drawing.Size(264, 44);
+            this.GetACat.TabIndex = 4;
+            this.GetACat.Text = "Get Cat image";
+            this.GetACat.UseVisualStyleBackColor = true;
+            this.GetACat.Click += new System.EventHandler(this.GetACat_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(963, 627);
+            this.Controls.Add(this.GetACat);
+            this.Controls.Add(this.catPicture);
             this.Controls.Add(this.GetADog);
             this.Controls.Add(this.dogPicture);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dogPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.catPicture)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +93,8 @@
 
         private System.Windows.Forms.PictureBox dogPicture;
         private System.Windows.Forms.Button GetADog;
+        private System.Windows.Forms.PictureBox catPicture;
+        private System.Windows.Forms.Button GetACat;
     }
 }
 
